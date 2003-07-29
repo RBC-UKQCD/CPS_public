c16b5e0939fa67b75eb5f665f8eeae16c8aada30
mcneile <mcneile@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2003-07-29 09:57:15 +0000

    I have added namespace support to the codes.
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@28 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/tests/f_hmd/main.C
+++ cps_pp/tests/f_hmd/main.C
@@ -1,15 +1,20 @@
 #include<config.h>
-CPS_START_NAMESPACE
 //--------------------------------------------------------------------
 //  CVS keywords
 //
 //  $Author: mcneile $
-//  $Date: 2003-06-22 13:34:49 $
-//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/f_hmd/main.C,v 1.1.1.1 2003-06-22 13:34:49 mcneile Exp $
-//  $Id: main.C,v 1.1.1.1 2003-06-22 13:34:49 mcneile Exp $
+//  $Date: 2003-07-29 09:57:15 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/f_hmd/main.C,v 1.2 2003-07-29 09:57:15 mcneile Exp $
+//  $Id: main.C,v 1.2 2003-07-29 09:57:15 mcneile Exp $
 //  $Name: not supported by cvs2svn $
 //  $Locker:  $
 //  $Log: not supported by cvs2svn $
+//  Revision 1.1.1.1  2003/06/22 13:34:49  mcneile
+//  This is the cleaned up version of the Columbia Physics System.
+//  The directory structure has been changed.
+//  The include paths have been updated.
+//
+//
 //  Revision 1.11  2002/12/04 17:16:27  zs
 //  Merged the new 2^4 RNG into the code.
 //  This new RNG is implemented in the LatRanGen class.
@@ -80,7 +85,7 @@ CPS_START_NAMESPACE
 //  Added CVS keywords to phys_v4_0_0_preCVS
 //
 //  $RCSfile: main.C,v $
-//  $Revision: 1.1.1.1 $
+//  $Revision: 1.2 $
 //  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/f_hmd/main.C,v $
 //  $State: Exp $
 //
@@ -89,7 +94,6 @@ CPS_START_NAMESPACE
  *  test for alg
  */
 
-CPS_END_NAMESPACE
 #include <stdio.h>
 #include <stdlib.h>	// exit()
 #include<config.h>
@@ -103,13 +107,17 @@ CPS_END_NAMESPACE
 #include<alg/cg_arg.h>
 #include<alg/hmd_arg.h>
 #include<alg/ghb_arg.h>
-CPS_START_NAMESPACE
 
-
+namespace cps
+{
 GlobalJobParameter GJP;
 LatRanGen LRG;
 Verbose VRB;
 Error ERR;
+}
+
+using namespace cps ;
+
 
 int main(int argc,char *argv[])
 {
@@ -347,4 +355,3 @@ int main(int argc,char *argv[])
   
 
 
-CPS_END_NAMESPACE
