27ff67bd7239000fef2b5d46995705989843be28
mcneile <mcneile@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-05-11 15:11:08 +0000

    These are some comparison functions for the testing framework.
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@491 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/util/testing_framework/testing_framework_cmp.C
@@ -0,0 +1,83 @@
+//--------------------------------------------------------------------
+//  Comparison testing utilities for the testing framework.
+//
+//
+//
+//  $Id: testing_framework_cmp.C,v 1.2 2004-05-11 15:11:08 mcneile Exp $
+//--------------------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>	// exit()
+#include <math.h>
+#include <config.h>
+#include <util/lattice.h>
+#include <util/gjp.h>
+#include <util/verbose.h>
+#include <util/error.h>
+#include <comms/glb.h>
+#include <alg/w_ginfo.h>
+
+CPS_START_NAMESPACE
+
+static inline int almost_zero(Float x)
+{
+  int ans ; 
+
+  if( fabs((double) x)  < 0.0000000000001 )
+    {
+      ans = 1 ;
+    }
+    else
+      { 
+	ans = 0 ; 
+      }
+
+
+  return ans ; 
+}
+
+
+void compare_array_relative(Float* pion_corr_A, 
+			    Float* pion_corr_B, 
+			    Float tol, 
+			    int time_size)
+{
+  printf("Start_regression_test\n") ; 
+
+  int pass_test = 1 ; 
+  for(int t= 0 ; t < time_size ; ++t)
+    {
+      Float pA = pion_corr_A[t] ; 
+      Float pB = pion_corr_B[t] ; 
+
+      Float rel ;
+      if( ! almost_zero(pA)  )
+	{
+	 rel = fabs( (pA-pB)/pA   ) ; 
+	}
+      else
+	{
+	 rel = fabs(pB) ; 
+	}
+      if( rel > tol) 
+	pass_test = 0 ; 
+
+      printf("|(%g - %g) / %g | = %g\n",pA,pB,pA,rel ) ; 
+    }
+
+  // write some text for the test
+    printf("test_type       = relative_float_array\n") ; 
+    printf("test_toleration = %g\n",tol) ; 
+
+  if (pass_test) 
+    printf("test_result = PASS\n") ; 
+  else 
+    printf("test_result = FAIL\n") ; 
+
+
+  printf("End_regression_test\n") ; 
+}
+
+
+CPS_END_NAMESPACE
