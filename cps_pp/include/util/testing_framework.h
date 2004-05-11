6cf028c6549de79e29a52ee1b731c730be641a09
mcneile <mcneile@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-05-11 15:07:35 +0000

    This is a header file for the testing framework.
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@488 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/include/util/testing_framework.h
@@ -0,0 +1,33 @@
+#ifndef TESTING_FRAMEWORK_INC
+#define TESTING_FRAMEWORK_INC
+
+#include<config.h>
+CPS_START_NAMESPACE
+
+//
+// Function prototypes for the testing framework
+//
+
+
+//
+//  ----- light weight hadron spectrum -----
+//
+
+void staggered_local_pion(Lattice &lat, Float mass, 
+			  IFloat* pion_corr, int time_size);
+
+//
+//  ----- comparison functions -----
+//
+
+void compare_array_relative(Float* pion_corr_A, 
+			    Float* pion_corr_B, 
+			    Float tol, 
+			    int time_size) ;
+
+
+
+CPS_END_NAMESPACE
+
+#endif
+
