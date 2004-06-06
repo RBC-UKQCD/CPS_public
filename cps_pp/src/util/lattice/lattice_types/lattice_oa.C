259aae710cb44b053626067ed4f8709579eac897
mcneile <mcneile@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-06-06 12:58:02 +0000

    Impliments the constructor/destructor for the asqtad one loop symanzik glue action
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@590 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/util/lattice/lattice_types/lattice_oa.C
@@ -0,0 +1,35 @@
+#include<config.h>
+CPS_START_NAMESPACE
+//--------------------------------------------------------------------
+/*!\file
+  \brief  Implementation of GimprOLSymFasqtad class.
+
+  $Id: lattice_oa.C,v 1.2 2004-06-06 12:58:02 mcneile Exp $
+*/
+//--------------------------------------------------------------------
+
+CPS_END_NAMESPACE
+#include <util/lattice.h>
+#include <util/verbose.h>
+CPS_START_NAMESPACE
+
+
+//------------------------------------------------------------------
+// One Loop Symanzik improved gauge action with Asqtad staggered fermion
+//------------------------------------------------------------------
+GimprOLSymFasqtad::GimprOLSymFasqtad()
+{
+  cname = "GimprOLSymFasqtad"; 
+  char *fname = "GimprOLSymFasqtad()";
+  VRB.Func(cname,fname);
+}
+
+GimprOLSymFasqtad::~GimprOLSymFasqtad()
+{
+    char *fname = "~GimprOLSymFasqtad()";
+    VRB.Func(cname,fname);
+}
+
+
+
+CPS_END_NAMESPACE
