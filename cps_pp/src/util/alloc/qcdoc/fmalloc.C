e461430db8d7dbf620ba5b4f337785eebdb848d2
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-07-01 20:49:20 +0000

    merging with qos-2-3-pre1-CJ
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@642 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/util/alloc/qcdoc/fmalloc.C
@@ -0,0 +1,45 @@
+#include<config.h>
+#include<string.h>
+CPS_START_NAMESPACE
+
+CPS_END_NAMESPACE
+#include <util/smalloc.h>
+#include <util/error.h>
+#include <util/verbose.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <qcdoc_align.h>
+#include <qalloc.h>
+CPS_START_NAMESPACE
+
+//! Allocate memory
+/*!
+  \param request The amount of memory (in bytes) to allocate
+  \return A pointer to the allocated memory
+*/
+void* fmalloc(int request){
+  void* ptr;
+  ptr = qalloc(QFAST,request);
+  if (ptr == NULL)
+  ptr = qalloc(QCOMMS,request);
+  if (ptr == NULL){
+  ERR.Pointer("","smalloc(i)","");
+  exit(1);
+  }
+  VRB.Smalloc("","fmalloc(i)","", ptr, request);
+  return ptr;
+}
+
+//! Free allocate memory
+/*!
+  \param p Pointer to the memory to be freed.
+*/
+
+void ffree(void* p){
+  VRB.Sfree("","sfree(v*)","",p);
+  qfree((char*) p);
+}
+
+
+
+CPS_END_NAMESPACE
