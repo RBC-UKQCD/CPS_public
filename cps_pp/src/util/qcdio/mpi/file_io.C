2c6f1b768fb9b17a9070ad32804c6e81b307bfec
zs <zs@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-08-18 11:45:33 +0000

    Made into wrappers of MPISCU functions
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@807 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/src/util/qcdio/mpi/file_io.C
+++ cps_pp/src/util/qcdio/mpi/file_io.C
@@ -2,35 +2,36 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <util/qcdio.h>
-#include <qalloc.h>
+
 CPS_START_NAMESPACE
+
 FILE *Fopen( FileIoType type, const char *filename, const char *mode){
     return fopen(filename,mode);
 }
 
 int Fclose( FileIoType type, FILE *stream){
-  return fclose(stream);
+    return fclose(stream);
 }
 
 int Fprintf( FileIoType type, FILE *stream, const char *format,...){
-  if ( type == ZERO_ONLY && UniqueID() != 1) return 0;
-  va_list args;
-  va_start(args,format);
-  int nbytes = vfprintf(stream,format,args);
-  va_end(args);
-  return nbytes;
+
+    va_list args;
+    va_start(args,format);
+    int nbytes = vfprintf(stream,format,args);
+    va_end(args);
+    return nbytes;
 }
 
 int Fprintf( FILE *stream, const char *format,...){
-  if ( UniqueID() != 1) return 0;
-  va_list args;
-  va_start(args,format);
-  int nbytes = vfprintf(stream,format,args);
-  va_end(args);
-  return nbytes;
+
+    va_list args;
+    va_start(args,format);
+    int nbytes = vfprintf(stream,format,args);
+    va_end(args);
+    return nbytes;
 }
 
 int Vfprintf( FileIoType type, FILE *stream, const char *format, va_list ap){
-  return vfprintf(stream, format, ap);
+    return vfprintf(stream, format, ap);
 }
 CPS_END_NAMESPACE
