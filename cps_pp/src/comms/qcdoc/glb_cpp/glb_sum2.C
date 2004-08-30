5773d0560057ed1d3f48a0f693107765961442a4
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-08-30 04:25:48 +0000

    internal routine for glb_cpp
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@943 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/comms/qcdoc/glb_cpp/glb_sum2.C
@@ -0,0 +1,122 @@
+#include<config.h>
+#include<qalloc.h>
+CPS_START_NAMESPACE
+//-------------------------------------------------------------------
+/*!\file
+  \brief  Definition of glb_sum_five routine.
+
+*/
+//--------------------------------------------------------------------
+//  CVS keywords
+//  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/comms/qcdoc/glb_cpp/glb_sum2.C,v $
+//  $State: Exp $
+//
+//--------------------------------------------------------------------
+//--------------------------------------------------------------
+//  glb_sum_internal2
+//
+//  An internal routine for glb_sum(), glb_sum_five()
+//--------------------------------------------------------------
+
+
+CPS_END_NAMESPACE
+#include<comms/glb.h>
+#include<comms/scu.h>
+#include<util/gjp.h>
+#include<comms/double64.h>
+#include <comms/sysfunc.h>
+CPS_START_NAMESPACE
+
+
+static Double64 *transmit_buf = NULL;
+static Double64 *receive_buf = NULL;
+static Double64 *gsum_buf = NULL;
+
+//----------------------------------------------------------------------
+/*!
+  This routine need only be used by domain-wall fermion code where
+  the 5th dimension is parallelised.
+  
+  \param float_p The number to be summed.
+  \post The number pointed to by \a float_p is summed over all nodes
+  and that sum is written back to \a float_p, which is identical on all nodes.
+
+  \ingroup comms
+*/
+//---------------------------------------------------------------------- 
+
+static int initted=0;
+static SCUDirArgIR *Send[5];
+static SCUDirArgIR *Recv[5];
+void glb_sum_internal2(Float * float_p,int ndir)
+{
+  static int NP[5] ={0,0,0,0,0};
+  static int coor[5] ={0,0,0,0,0};
+
+  if (!initted){
+      NP[0] = GJP.Xnodes();
+      int max=NP[0];
+      NP[1] = GJP.Ynodes();
+      NP[2] = GJP.Znodes();
+      NP[3] = GJP.Tnodes();
+      NP[4] = GJP.Snodes();
+      for (int i = 1;i<5;i++)
+	if (max <NP[i]) max = NP[i];
+      transmit_buf = (Double64 *)qalloc(QFAST|QNONCACHE,sizeof(Double64)*2);
+      receive_buf = transmit_buf+1;
+      gsum_buf = (Double64 *)qalloc(QFAST,sizeof(Double64)*max);
+      for(int i = 0;i<5;i++){
+      Send[i] = new SCUDirArgIR(transmit_buf, gjp_scu_dir[2*i+1], SCU_SEND, sizeof(Double64));
+      Recv[i] = new SCUDirArgIR(receive_buf, gjp_scu_dir[2*i], SCU_REC, sizeof(Double64));
+      }
+  }
+  initted = 1;
+
+  // Sum over the "virtual" 5-dimensional mesh
+  //------------------------------------------------------------
+//  gsum_buf[0] = (Double64)*float_p;
+
+  Double64 tmp_sum = (Double64)*float_p;
+  
+  for(int i = 0; i < ndir; ++i) 
+  if (NP[i] >1) {
+      int coor = GJP.NodeCoor(i);
+//printf("coor[%d]=%d\n",i,coor);
+      *transmit_buf = gsum_buf[coor]= tmp_sum;
+
+      for (int itmp = 1; itmp < NP[i]; itmp++) {
+	coor = (coor+1)%NP[i];
+	Send[i]->StartTrans(); Recv[i]->StartTrans();
+	Send[i]->TransComplete(); Recv[i]->TransComplete();
+
+        gsum_buf[coor] = *receive_buf;
+        *transmit_buf = *receive_buf;
+      }
+      tmp_sum = gsum_buf[0];
+      for (int itmp = 1; itmp < NP[i]; itmp++) {
+	    tmp_sum += gsum_buf[itmp];
+      }
+  }
+  *float_p = (Float)tmp_sum;
+
+#if 0
+  *transmit_buf = *gsum_buf;
+  for(i = 0; i < ndir; ++i) 
+  if ( NP[i] >1){ 
+      	int coor = GJP.NodeCoor[i];
+	if (coor ==0 )Send[i]->StartTrans();Send[i] ->TransComplete();
+	else {
+		Recv[i]->StartTrans(); Recv[i]->TransComplete();
+		if (GJP.NodeCoor[i] <NP[i]-1 ){
+			Send[i]->StartTrans(); Send[i]->TransComplete();
+		}
+        	*transmit_buf = *receive_buf;
+	}
+
+  }
+  *float_p = (Float)*transmit_buf;
+#endif
+}
+
+
+CPS_END_NAMESPACE
