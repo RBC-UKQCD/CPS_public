5c6379ca64ba8be4c012a758f452bf3a2e7f3f87
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-08-30 06:30:52 +0000

    *** empty log message ***
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@956 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/util/gjp/qcdoc/start_end.C
@@ -0,0 +1,12 @@
+#include <config.h>
+#include <util/gjp.h>
+
+extern "C" void _mcleanup();
+CPS_START_NAMESPACE
+void Start(){
+// DefaultSetup();
+}
+void End(){
+  _mcleanup();
+}
+CPS_END_NAMESPACE
