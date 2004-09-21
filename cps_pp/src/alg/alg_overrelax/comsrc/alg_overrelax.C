c0319e65f7272a6bc6fe1d2002e9137b59c31f69
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-09-21 18:08:21 +0000

    Adding Kostya Petrov's overrelaxation code
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1063 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/alg/alg_overrelax/comsrc/alg_overrelax.C
@@ -0,0 +1,209 @@
+#include<config.h>
+CPS_START_NAMESPACE
+/*!\file
+  \brief Definitions of the AlgOverRelax class methods.
+  
+  $Id: alg_overrelax.C,v 1.2 2004-09-21 18:08:20 chulwoo Exp $
+*/
+//--------------------------------------------------------------------
+//  CVS keywords
+//
+//  $Author: chulwoo $
+//  $Date: 2004-09-21 18:08:20 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/alg/alg_overrelax/comsrc/alg_overrelax.C,v 1.2 2004-09-21 18:08:20 chulwoo Exp $
+//  $Id: alg_overrelax.C,v 1.2 2004-09-21 18:08:20 chulwoo Exp $
+//  $Name: not supported by cvs2svn $
+//  $Locker:  $
+//  $RCSfile: alg_overrelax.C,v $
+//  $Revision: 1.2 $
+//  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/alg/alg_overrelax/comsrc/alg_overrelax.C,v $
+//  $State: Exp $
+//
+
+CPS_END_NAMESPACE
+#include <stdlib.h>	// exit()
+#include <util/qcdio.h>
+#include <math.h>
+#include <time.h>
+#include <alg/alg_overrelax.h>
+#include <alg/common_arg.h>
+#include <alg/overrelax_arg.h>
+#include <util/lattice.h>
+#include <util/gjp.h>
+#include <util/random.h>
+#include <util/smalloc.h>
+#include <util/vector.h>
+#include <util/verbose.h>
+#include <util/error.h>
+
+CPS_START_NAMESPACE
+
+//Uncomment next line to switch on timing
+//#define OVERRELAX_TIMING
+
+//------------------------------------------------------------------
+/*!
+  \param latt The lattice on which to perform the heatbath
+  \param c_arg The common argument structure for all algorithms.
+  \param arg The parameters specific to this algorithm.
+ */
+//------------------------------------------------------------------
+AlgOverRelax::AlgOverRelax(Lattice& latt, 
+	     CommonArg *c_arg,
+	     OverRelaxArg *arg) : 
+	     Alg(latt, c_arg) 
+{
+  cname = "AlgOverRelax";
+  char *fname = "AlgOverRelax(L&,CommonArg*,OverrelaxArg*)";
+  VRB.Func(cname,fname);
+
+  // Initialize the argument pointer
+  //----------------------------------------------------------------
+  if(arg == 0)   ERR.Pointer(cname,fname, "arg");
+  alg_overrelax_arg = arg;
+  fGamma = (2./3.) * GJP.Beta();
+
+}
+
+
+//------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------
+AlgOverRelax::~AlgOverRelax() {
+  //char *fname = "~AlgOverRelax()";
+  //VRB.Func(cname,fname);
+}
+
+//------------------------------------------------------------------
+// AlgOverRelax::run()
+//  only useful for the naive wilson gauge action. 
+//  checkerboard over nodes is not done.
+
+//! Run the overrelaxation algorithm for the Wilson (plaquette) gauge action.
+//------------------------------------------------------------------
+void AlgOverRelax::run()
+{
+  char *fname = "run()";
+  VRB.Func(cname,fname);
+
+  // Set the Lattice pointer
+  //----------------------------------------------------------------
+  Lattice& lat = AlgLattice();
+
+  LRG.SetInterval(1,-1);
+int x[4], y[4];
+  Matrix mStaple;
+  // Run the gauge heat bath
+  //----------------------------------------------------------------
+  for(int i=0; i< alg_overrelax_arg->num_iter; i++){
+
+     // Over Relaxation
+     // Checkerboard everything, do even or odd sites
+     // Scan over local subvolume doing all even(odd) links
+     // index for "x" is consistant with GsiteOffset: "x,y,z,t" order
+
+	// The y[4] are the local coordinates for the 2^4 cube. 
+	// We traverse the points on this cube, and do the corresponding
+	// points on every other hypercube before moving on.
+
+  
+  for( int checker=0; checker < 2; checker++)  {
+    for( y[3] = 0; y[3] < 2; y[3]++) 		
+    for( y[2] = 0; y[2] < 2; y[2]++)
+    for( y[1] = 0; y[1] < 2; y[1]++) 		
+    for( y[0] = 0; y[0] < 2; y[0]++) 		
+      if( (y[0]+y[1]+y[2]+y[3])%2 == checker)  {
+
+        for( x[3] = y[3]; x[3] < GJP.TnodeSites(); x[3]+=2) 
+        for( x[2] = y[2]; x[2] < GJP.ZnodeSites(); x[2]+=2) 
+        for( x[1] = y[1]; x[1] < GJP.YnodeSites(); x[1]+=2) 
+        for( x[0] = y[0]; x[0] < GJP.XnodeSites(); x[0]+=2) 
+          for( int mu=0; mu<4; mu++ ) {
+
+            LRG.AssignGenerator(x);
+
+#ifdef OVERRELAX_TIMING
+	    int clock1 = clock();
+#endif
+
+       	    // Build the staple:
+            lat.Staple( mStaple, x, mu );
+
+            Matrix * pmLink = lat.GaugeField();
+            pmLink += mu + lat.GsiteOffset(x);
+	    //   printf("wqw %le\n", (*pmLink).ErrorSU3());
+	        UpdateLink(pmLink, mStaple);
+
+#ifdef OVERRELAX_TIMING
+    	    printf("OVER: one link update time %d\n", clock()-clock1);
+#endif	
+          }			// (end mu loop)
+        }           		// (end of site loop)
+      }				// (end checkerboard loop)
+    
+
+    // Increment gauge field counter
+    //----------------------------------------------------------------
+    lat.GupdCntInc(1);
+
+    // If GJP.Snodes() !=1  the gauge field is spread out
+    // accross s-slices of processors. It must be identical
+    // on each slice. Check to make sure and exit if it
+    // is not identical. A case where this is relevant
+    // is the DWF spread-out case.
+    //----------------------------------------------------------------
+    //     lat.GsoCheck();
+    //    cout<<".";
+  }
+
+  // Reunitarize
+  //----------------------------------------------------------------
+  lat.Reunitarize();
+  //VRB.FuncEnd(fname, cname);   
+}
+//Matrix mirror(Matrix  & Link, const  Matrix & Staple)
+//{
+  //U=Dagger(Staple*Link*Staple);
+
+//}
+void AlgOverRelax::UpdateLink(Matrix  * pmLink, const  Matrix & mStaple)
+{
+  
+   Float  old_action,  new_action;
+  Float  accept_probability;
+  Float  die_roll;
+
+  Matrix Trial, Snew,Sold, Temp1, Temp2;
+  // remember - you cannot use multi-hit here, it breaks detailed balance
+  //generate G0xGxG0
+  Trial.Dagger(* pmLink);
+  Temp1.DotMEqual(mStaple, Trial);
+  Temp2.DotMEqual(Temp1,mStaple);
+
+  //  printf("qqq %le %le %le\n",(* pmLink).ErrorSU3(),mStaple.ErrorSU3(),Trial.ErrorSU3());
+  Sold.DotMEqual(*pmLink,mStaple);
+  Snew.DotMEqual(Trial,mStaple);
+
+  
+  // Now begin the update process. 
+  
+  old_action = fGamma * (9. - .5 * Sold.ReTr() );
+  
+  new_action = fGamma * (9. - .5 * Snew.ReTr());
+  
+  accept_probability = exp( old_action - new_action );
+  
+  
+  if(abs(LRG.Urand()) < accept_probability ) {    
+    for(int i=0;i<18;i++){ 
+           *((Float*)pmLink +  i ) =Trial.elem(i);
+
+   }
+        
+  }
+  
+}
+
+
+
+CPS_END_NAMESPACE
---
+++ cps_pp/src/alg/alg_overrelax/noarch/alg_overrelax_kern.C
@@ -0,0 +1,106 @@
+#include<config.h>
+CPS_START_NAMESPACE
+//--------------------------------------------------------------------
+/*!\file
+  \brief Routines used by the AlgOverRelax class methods:
+
+  $Id: alg_overrelax_kern.C,v 1.2 2004-09-21 18:08:21 chulwoo Exp $
+*/
+//--------------------------------------------------------------------
+//  CVS keywords
+//
+//  $Author: chulwoo $
+//  $Date: 2004-09-21 18:08:21 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/alg/alg_overrelax/noarch/alg_overrelax_kern.C,v 1.2 2004-09-21 18:08:21 chulwoo Exp $
+//  $Id: alg_overrelax_kern.C,v 1.2 2004-09-21 18:08:21 chulwoo Exp $
+//  $Name: not supported by cvs2svn $
+//  $Locker:  $
+//  $RCSfile: alg_overrelax_kern.C,v $
+//  $Revision: 1.2 $
+//  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/alg/alg_overrelax/noarch/alg_overrelax_kern.C,v $
+//  $State: Exp $
+//
+//--------------------------------------------------------------------
+//------------------------------------------------------------------
+//
+// alg_overrelax.C
+//
+// AlgOverRelax is derived from Alg and is relevant to the 
+// gauge overrelaxation algorithm. The type of gauge action is
+// determined by the argument to the constructor.
+//
+//------------------------------------------------------------------
+
+CPS_END_NAMESPACE
+#include <stdlib.h>	// exit()
+#include <util/qcdio.h>
+#include <math.h>
+#include<alg/alg_overrelax.h>
+#include<util/lattice.h>
+#include<util/gjp.h>
+#include<util/random.h>
+#include<util/smalloc.h>
+#include<util/vector.h>
+#include<util/verbose.h>
+#include<util/error.h>
+#include<util/algebra.h>
+CPS_START_NAMESPACE
+
+
+
+
+
+static int	NHITS = 10;
+static Float	SMALL = 0.2;
+static Float	EPSILON[MATRIXSIZE];
+static Float	MTEMP1[MATRIXSIZE];
+static Float	u_sigma[MATRIXSIZE];
+
+//*******************************************************
+//     Here is a Metropolis update kernel...  		*
+//*******************************************************
+void AlgOverRelax::kernel(  Float *sigma, Float *U)
+{
+
+ 
+  // These variables are associated with the actual update. 
+  Float  old_action;
+  Float  new_action;
+  Float  accept_probability;
+  Float  die_roll;
+  
+
+  
+  // remember - you cannot use multi-hit here, it breaks detailed balance
+  //generate G0xGxG0
+  m_multiply3( u_sigma, U, sigma );
+  m_multiply2r( u_sigma, sigma );
+  m_conjugate(u_sigma);
+  //yes I know this is slow
+  
+  
+  // Now begin the update process. 
+  
+  m_multiply3( MTEMP1, sigma, U );
+  old_action = fGamma * (9. - .5 * (MTEMP1[0]+MTEMP1[4]+MTEMP1[8]) );
+    
+  m_multiply3( MTEMP1, u_sigma, U );
+  
+  new_action = fGamma * (9. - .5 * (MTEMP1[0]+MTEMP1[4]+MTEMP1[8]) );
+  
+  accept_probability = exp( old_action - new_action );
+  
+  
+  if(abs(LRG.Urand()) < accept_probability ) {
+   
+    
+    m_equal( U, u_sigma );
+  }
+
+  
+}             /* End HIT_CNT loop for multiple hits.           */
+
+
+
+
+CPS_END_NAMESPACE
