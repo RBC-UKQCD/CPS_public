9a8ff1fca9ac82cafea19f01399e00ee8d429cd6
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-11-07 23:52:47 +0000

    change to compile with GCC 3.4.1
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1208 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/include/util/amalloc.h
+++ cps_pp/include/util/amalloc.h
@@ -2,7 +2,7 @@
 /*!\file
   \brief  Declaration of dynamic memory allocation routine for arrays.
 
-  $Id: amalloc.h,v 1.4 2004-10-27 14:30:25 zs Exp $
+  $Id: amalloc.h,v 1.5 2004-11-07 23:52:47 chulwoo Exp $
 */
 //--------------------------------------------------------------------
 #ifndef AMALLOC_H
@@ -11,8 +11,8 @@
 
 CPS_START_NAMESPACE
 
-void* amalloc(void*  (*allocator)(size_t, const char *vname="",
-			  const char *fname="smalloc", const char *cname=""),
+void* amalloc(void*  (*allocator)(size_t, const char *vname,
+			  const char *fname, const char *cname),
 	      size_t, int, ...);
 
 CPS_END_NAMESPACE
