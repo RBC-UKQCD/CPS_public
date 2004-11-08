17a839eb611178c196ce96d0f4b79bda15ffca0d
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-11-08 18:33:10 +0000

    fixed (again!) to compile with XLC
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1219 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/src/util/lattice/f_dwf_base/f_dwf_base.C
+++ cps_pp/src/util/lattice/f_dwf_base/f_dwf_base.C
@@ -214,13 +214,16 @@ int FdwfBase::FmatEvlMInv(Vector **f_out, Vector *f_in, Float *shift,
 
   int f_size = GJP.VolNodeSites() * FsiteSize() / (FchkbEvl()+1);
   Float dot = f_in -> NormSqGlbSum(f_size);
-  Float RsdCG[Nshift];
+//  Float RsdCG[Nshift];
+  Float *RsdCG = new Float[Nshift];
   for (int s=0; s<Nshift; s++) RsdCG[s] = cg_arg->stop_rsd;
 
   //Fake the constructor
   DiracOpDwf dwf(*this, f_out[0], f_in, cg_arg, cnv_frm);
   cg_arg->true_rsd = RsdCG[isz];
-  return dwf.MInvCG(f_out,f_in,dot,shift,Nshift,isz,RsdCG,type,alpha);  
+  int return_value =  dwf.MInvCG(f_out,f_in,dot,shift,Nshift,isz,RsdCG,type,alpha);  
+  delete[] RsdCG;
+  return return_value;
 }
 
 //------------------------------------------------------------------
