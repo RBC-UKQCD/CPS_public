077f624130ad25dfa275e740c43b062776cb3657
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2004-12-01 07:34:58 +0000

    *** empty log message ***
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1276 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/tests/g_hb/main.C
+++ cps_pp/tests/g_hb/main.C
@@ -3,13 +3,13 @@
 //  CVS keywords
 //
 //  $Author: chulwoo $
-//  $Date: 2004-09-21 20:16:54 $
-//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/g_hb/main.C,v 1.7 2004-09-21 20:16:54 chulwoo Exp $
-//  $Id: main.C,v 1.7 2004-09-21 20:16:54 chulwoo Exp $
+//  $Date: 2004-12-01 07:34:44 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/g_hb/main.C,v 1.8 2004-12-01 07:34:44 chulwoo Exp $
+//  $Id: main.C,v 1.8 2004-12-01 07:34:44 chulwoo Exp $
 //  $Name: not supported by cvs2svn $
 //  $Locker:  $
 //  $RCSfile: main.C,v $
-//  $Revision: 1.7 $
+//  $Revision: 1.8 $
 //  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/tests/g_hb/main.C,v $
 //  $State: Exp $
 //
@@ -20,6 +20,7 @@
 #include<alg/alg_ghb.h>
 #include<alg/no_arg.h>
 #include<alg/alg_plaq.h>
+#include<comms/sysfunc.h>
 
 
 USING_NAMESPACE_CPS
@@ -27,38 +28,34 @@ USING_NAMESPACE_CPS
 
 int main(int argc,char *argv[])
 {
-  int ITERATIONS = 1;
+  int ITERATIONS = 10;
 
   //----------------------------------------------------------------
   // Initializes all Global Job Parameters
   //----------------------------------------------------------------
+  Start();
   DoArg do_arg;
 
-  //-----------------------------------------------------
-  // Timing results:
-  // 20 it, 16^3x4 on 1mb 75sec (w/asm)
-  // 20 it, 16^3x4 on 1mb 45sec (w/cram w/asm)
-  // 20 it, 16^3x4 on 1mb 37sec (w/o cmhb kernal)
-  // 20 it, 16^3x4 on 1mb  8sec (w/o update - action only)
-  do_arg.t_node_sites = 2;
-  do_arg.x_node_sites = 2;
-  do_arg.y_node_sites = 2;
-  do_arg.z_node_sites = 2;
+  int nx,ny,nz,nt;
+  if (argc<5) {printf("usage: %s nx ny nz nt\n",argv[0]);exit(-2);}
+  sscanf(argv[1],"%d",&nx);
+  sscanf(argv[2],"%d",&ny);
+  sscanf(argv[3],"%d",&nz);
+  sscanf(argv[4],"%d",&nt);
+
+  printf("sizes = %d %d %d %d\n",nx,ny,nz,nt);
+  do_arg.x_node_sites = nx/SizeX();
+  do_arg.y_node_sites = ny/SizeY();
+  do_arg.z_node_sites = nz/SizeZ();
+  do_arg.t_node_sites = nt/SizeT();
   do_arg.s_node_sites = 2;
 
-#ifdef PARALLEL
-  do_arg.t_nodes = 2;
-  do_arg.x_nodes = 2;
-  do_arg.y_nodes = 2;
-  do_arg.z_nodes = 2;
+  do_arg.x_nodes = SizeX();
+  do_arg.y_nodes = SizeY();
+  do_arg.z_nodes = SizeZ();
+  do_arg.t_nodes = SizeT();
   do_arg.s_nodes = 1;
-#else
-  do_arg.x_nodes = 1;
-  do_arg.y_nodes = 1;
-  do_arg.z_nodes = 1;
-  do_arg.t_nodes = 1;
-  do_arg.s_nodes = 1;
-#endif 
+
   do_arg.t_bc = BND_CND_PRD;
   do_arg.x_bc = BND_CND_PRD;
   do_arg.y_bc = BND_CND_PRD;
