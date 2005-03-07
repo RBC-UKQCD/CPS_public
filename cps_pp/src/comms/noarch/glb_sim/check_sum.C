19e214d4cc6d521bb3188221404f9c85a1494be3
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2005-03-07 17:55:32 +0000

    *** empty log message ***
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1514 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/comms/noarch/glb_sim/check_sum.C
@@ -0,0 +1,57 @@
+#include<config.h>
+#include<qalloc.h>
+CPS_START_NAMESPACE
+//-------------------------------------------------------------------
+/*!\file
+  \brief  Definition of local_checksum and global_checksum outine.
+
+*/
+//--------------------------------------------------------------------
+//  CVS keywords
+//  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/comms/noarch/glb_sim/check_sum.C,v $
+//  $State: Exp $
+//
+//--------------------------------------------------------------------
+//--------------------------------------------------------------
+//  local_checksum
+//
+//  Calculating checksum on a set of data on one node
+//
+//  global_checksum
+//
+//  Calculating checksum on each node and compare against each other
+//--------------------------------------------------------------
+
+
+CPS_END_NAMESPACE
+#include <util/data_types.h>
+#include<comms/glb.h>
+#include "glb_sum_internal.h"
+//#include<comms/scu.h>
+//#include<util/gjp.h>
+//#include<comms/double64.h>
+//#include <comms/sysfunc.h>
+CPS_START_NAMESPACE
+
+unsigned int local_checksum(Float * float_p, int len) {
+  unsigned int csum = 0;
+  unsigned int * uint_p = (unsigned int *) float_p;
+  for(int i=0;i < len * sizeof(Float) / sizeof(unsigned int); i++) {
+//    csum += *uint_p;
+    csum ^= *uint_p;
+    uint_p ++;
+  }
+  return csum;
+}
+
+unsigned int global_checksum(Float * float_p, int len) {
+  ERR.NotImplemented("","global_checksum(Float *, int)");
+  return 0;
+}
+
+unsigned int test_checksum(Float * float_p, int len) {
+  ERR.NotImplemented("","test_checksum(Float *, int)");
+  return 0;
+}
+
+CPS_END_NAMESPACE
