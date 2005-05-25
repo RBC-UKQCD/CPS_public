17f7c3aa1cd8337e91699ff2f998a9e283c12ba5
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2005-05-25 17:10:53 +0000

    Merging v4_8_14b_P4
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1766 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/util/lattice/f_p4/noarch/force_product_sum_p4.C
@@ -0,0 +1,178 @@
+#include<config.h>
+
+/*!\file
+  \brief Helper routines for the fermionic force term
+
+  $Id: force_product_sum_p4.C,v 1.2 2005-05-25 17:08:35 chulwoo Exp $
+*/
+//--------------------------------------------------------------------
+
+#include <stdio.h>
+#include <util/lattice.h>
+#include <util/gjp.h>
+
+USING_NAMESPACE_CPS
+
+extern "C" void Force_cross2dag(const Vector *chi, const Vector *phi, Matrix *result, int counter, double *fac);
+
+// f += coeff * U_dir(x) * v(x)^dagger
+// Note that STAG order stores hermitian conjugate of links.
+
+void Fp4::force_product_sum(const Matrix *v,  int dir,
+				IFloat coeff, Matrix *f){
+
+  Matrix m, md;
+  int n;
+
+  int s[4];
+  for(s[3]=0; s[3]<node_sites[3]; s[3]++)
+    for(s[2]=0; s[2]<node_sites[2]; s[2]++)
+      for(s[1]=0; s[1]<node_sites[1]; s[1]++) {
+	for(s[0]=0; s[0]<node_sites[0]; s[0]++){
+	  n = FsiteOffset(s);
+	  m.DotMEqual(v[n], *(GaugeField()+GsiteOffset(s)+dir));
+	  md.Dagger(m);
+	  md *= coeff;
+	  f[n] += md;
+	}
+      }
+
+  int vol = node_sites[0]*node_sites[1]*node_sites[2]*node_sites[3];
+  ForceFlops += vol * 234;
+
+}
+
+
+// f += u(vw)^dagger
+
+void Fp4::force_product_sum(const Matrix *u, const Matrix *v,
+				const Matrix *w, IFloat coeff, 
+				Matrix *f, Matrix *tmp){
+
+  Matrix m, md;
+  int n;
+
+  int s[4];
+  for(s[3]=0; s[3]<node_sites[3]; s[3]++)
+    for(s[2]=0; s[2]<node_sites[2]; s[2]++)
+      for(s[1]=0; s[1]<node_sites[1]; s[1]++) {
+	for(s[0]=0; s[0]<node_sites[0]; s[0]++){
+	  n = FsiteOffset(s);
+	  m.DotMEqual(v[n], w[n]);
+	  md.Dagger(m);
+	  md *= coeff;
+	  f[n].DotMPlus(u[n], md);
+	}    
+      }
+
+  int vol = node_sites[0]*node_sites[1]*node_sites[2]*node_sites[3];
+  ForceFlops += vol * 432;
+
+}
+
+
+// f += coeff v w^dagger
+
+void Fp4::force_product_sum(const Matrix *v, const Matrix *w,
+				IFloat coeff, Matrix *f){
+
+  Matrix m, md;
+  int n;
+
+  int s[4];
+  for(s[3]=0; s[3]<node_sites[3]; s[3]++)
+    for(s[2]=0; s[2]<node_sites[2]; s[2]++)
+      for(s[1]=0; s[1]<node_sites[1]; s[1]++) {
+	for(s[0]=0; s[0]<node_sites[0]; s[0]++){
+	  n = FsiteOffset(s);
+	  md.Dagger(w[n]);
+	  m.DotMEqual(v[n], md);
+	  m *= coeff;
+	  f[n] += m;
+	}
+      }
+
+  int vol = node_sites[0]*node_sites[1]*node_sites[2]*node_sites[3];
+  ForceFlops += vol * 234;
+
+}
+
+// f += coeff (v w)^dagger
+
+void Fp4::force_product_d_sum(const Matrix *v, const Matrix *w,
+				    IFloat coeff, Matrix *f){
+
+  Matrix m, md;
+  int n;
+  
+  int s[4];
+  for(s[3]=0; s[3]<node_sites[3]; s[3]++)
+    for(s[2]=0; s[2]<node_sites[2]; s[2]++)
+      for(s[1]=0; s[1]<node_sites[1]; s[1]++) {
+	for(s[0]=0; s[0]<node_sites[0]; s[0]++) {
+	  n = FsiteOffset(s);
+	  m.DotMEqual(v[n], w[n]);
+	  md.Dagger(m);
+	  md *= coeff;
+	  f[n] += md;
+	}
+      }
+
+  int vol = node_sites[0]*node_sites[1]*node_sites[2]*node_sites[3];
+  ForceFlops += vol * 234;
+
+}
+
+
+
+// The outer product of v and w, multiplied by the parity factor and
+// coeffient, and added to the force f.
+// N.B. The force term is multiplied by -1 on ODD parity sites. This the
+// the MILC convention and is here to test against the MILC code.
+// This should eventually be changed to EVEN for the CPS.
+
+void Fp4::force_product_sum(const Vector *v, const Vector *w,
+				    IFloat coeff, Matrix *f){
+
+  int vol = GJP.XnodeSites() * GJP.YnodeSites() * GJP.ZnodeSites() * GJP.TnodeSites() ;
+  ForceFlops +=78*vol;
+
+  Matrix m;
+  int n, s[4];
+  
+  //    printf("force_product_sum\n");
+  for(s[3]=0; s[3]<GJP.TnodeSites(); s[3]++)
+    for(s[2]=0; s[2]<GJP.ZnodeSites(); s[2]++)
+      for(s[1]=0; s[1]<GJP.YnodeSites(); s[1]++)
+	for(s[0]=0; s[0]<GJP.XnodeSites(); s[0]++){
+	  n = FsiteOffset(s);
+	  m.Cross2(v[n], w[n]);
+	  // Note the extra factor of 2 from Matrix::Cross2; this is corrected by
+	  // the factor of 1/2 in Matrix::TrLessAntiHermMatrix used in
+	  // Fp4::update_momenta.
+#if 0
+	  if(parity(s)==CHKB_ODD) m *= -coeff;    
+	  else m *= coeff;
+#else
+	  m *= coeff;
+#endif
+	  f[n] += m;
+	  //		printf("%d %d %d %d f{%d]=%p\n",s[0],s[1],s[2],s[3],n,&(f[n]));
+	}
+#if 0
+  {IFloat *tmp = (IFloat *)f;
+  printf("result[0]=");
+  for(int i = 0;i<18;i++){
+    printf(" %0.8e",*(tmp++));
+    if(i%6==5) printf("\n");
+  }
+  tmp = (IFloat *)&(f[vol-1]);
+  printf("result[%d]=",vol-1);
+  for(int i = 0;i<18;i++){
+    printf(" %0.8e",*(tmp++));
+    if(i%6==5) printf("\n");
+  }
+  }
+  exit(54);
+#endif
+}
---
+++ cps_pp/src/util/lattice/g_tadpole_rect/qcdoc/g_tadpole_rect_force.C
@@ -0,0 +1,241 @@
+//Hybrid Monte Carlo force term for an improved gauge action with plaquette
+//and rectangle terms.  Takes advantage of lattice-wide operations to speed
+//evolution of HMC trajectory.
+
+#include <config.h>
+#include <math.h>
+#include <util/gjp.h>
+#include <util/lattice.h>
+#include <util/smalloc.h>
+#include <util/pt.h>
+#include <util/time.h>
+CPS_START_NAMESPACE
+
+// I don't know what this does
+#define PROFILE
+//------------------------------------------------------------------
+// EvolveMomGforce(Matrix *mom, Float step_size):
+// It evolves the canonical momentum mom by step_size
+// using the pure gauge force.
+//------------------------------------------------------------------
+
+void GtadpoleRect::EvolveMomGforce(Matrix *mom, Float step_size)
+{
+  char *fname = "EvolveMomGforce(M*,F)mod";  //Name of our function
+  VRB.Func(cname,fname);                     //Sets name of the function
+
+  static Matrix mt0;
+  static Matrix *mp0 = &mt0;
+
+  //Sets some kind of flop counter
+#ifdef PROFILE
+  Float time = -dclock();
+  ForceFlops = 0;
+  ParTrans::PTflops=0;
+#endif
+
+  static int vol = GJP.VolNodeSites();  //Local lattice volume
+  const int N = 4;                      //Num of dimensions
+  Float tmp_plaq = plaq_coeff;          //1-8*c_1
+  Float tmp_rect = rect_coeff;          //c_1/(u0*u0)
+
+  //Pointer to block of unit matrices for each lattice site
+  Matrix *Unit = (Matrix *) fmalloc(vol*sizeof(Matrix));
+
+  //Set all these matrices to the identity
+  for(int i = 0; i < vol;i++)
+    Unit[i] = 1.;
+
+  //Temporary matrices for use in calculation of the staples
+  Matrix *tmp1[N];
+  Matrix *tmp2[N];
+
+  for(int i = 0;i<N;i++)
+    {
+      tmp1[i] = (Matrix *) fmalloc(vol*sizeof(Matrix));
+      tmp2[i] = (Matrix *) fmalloc(vol*sizeof(Matrix));
+      //Set all bytes in tmp2 to zero
+      bzero((char*)tmp2[i],vol*sizeof(Matrix));
+    }
+
+  //Holds the sum of staples associated with each lattice site
+  Matrix *result[4];
+  for(int i = 0;i<4;i++){
+    result[i] = (Matrix *) fmalloc(vol*sizeof(Matrix));
+  }
+
+  //Array of four pointers to a block of unit matrices
+  Matrix *Units[4];
+  for(int i = 0; i < N;i++)
+    Units[i] = Unit;
+
+  int mu,nu;
+  {
+    int dirs_p[] = {0,2,4,6,0,2,4};   //Positive directions
+    int dirs_m[] = {1,3,5,7,1,3,5};   //Negative directions
+
+    //Instantiate parallel transporter
+    ParTransGauge pt(*this);
+
+    //Take a vector field V(x).  Suppose we wish to parallel transport
+    //this vector field from the point x to the point x-mu.  That is,
+    //we wish to find a new field V'(x-mu) in terms of V(x) such that
+    //the new field V'(x-mu) changes appropriately under gauge transformation.
+    //
+    //The parallel transport that satisfies this property is
+    //V'(x-mu) = U_mu(x-mu)V(x).  This combination transforms like
+    //a vector field.
+    //
+    //When using the parallel transport class, one must specify an intial
+    //field, a direction, and a final field.  However, the direction
+    //that one must specify is the direction of the link matrix, not the
+    //direction of the parallel transport.  Thus, to calculate V' as above,
+    //use:
+    //
+    //pt.run(1,V',V,dir_Plus_mu)
+    //
+    //The new vector field will be indexed according to its new position.
+      for(nu = 1;nu<4;nu++){
+
+	//First calculate the staple in the positive nu direction
+        pt.run(N,tmp1,Units,dirs_m+nu);
+  	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_p+nu);
+
+	//tmp2 contains the sum of the staples for a given link
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_plaq,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating one rectangular staple
+	pt.run(N,tmp1,Units,dirs_p);
+	pt.run(N,result,tmp1,dirs_m+nu);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_p+nu);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating another rectangular staple;
+	pt.run(N,tmp1,Units,dirs_m+nu);
+	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_p+nu);
+	pt.run(N,tmp1,result,dirs_p);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating another rectangular staple;
+	pt.run(N,tmp1,Units,dirs_m+nu);
+	pt.run(N,result,tmp1,dirs_m+nu);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_p+nu);
+	pt.run(N,tmp1,result,dirs_p+nu);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating the staple in the negative nu direction
+	pt.run(N,tmp1,Units,dirs_p+nu);
+	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_m+nu);
+
+	//Add this result into tmp2
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_plaq,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating one rectangular staple
+	pt.run(N,tmp1,Units,dirs_p);
+	pt.run(N,result,tmp1,dirs_p+nu);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_m+nu);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating another rectangular staple;
+	pt.run(N,tmp1,Units,dirs_p+nu);
+	pt.run(N,result,tmp1,dirs_m);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_m+nu);
+	pt.run(N,tmp1,result,dirs_p);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Calculating another rectangular staple;
+	pt.run(N,tmp1,Units,dirs_p+nu);
+	pt.run(N,result,tmp1,dirs_p+nu);
+	pt.run(N,tmp1,result,dirs_m);
+	pt.run(N,result,tmp1,dirs_m+nu);
+	pt.run(N,tmp1,result,dirs_m+nu);
+
+	for(int i = 0; i<N;i++)
+	vaxpy3_m(tmp2[i],&tmp_rect,tmp1[i],tmp2[i],vol*3);
+
+	//Count the flops?
+        ForceFlops +=vol*288*N;
+      }
+      //Multiply on the left by our original link matrix to get force term
+      pt.run(N,result,tmp2,dirs_p);
+  }
+
+      Matrix mp1;
+      for(mu = 0; mu<4;mu++)
+	{
+	  Matrix *mtmp = result[mu];
+	  // Takes TrLessAntiHerm part to get the force
+	  for(int i = 0; i<vol;i++)
+	    {
+	      #if 1
+	      mp1.Dagger((IFloat *)mtmp);
+	      mtmp->TrLessAntiHermMatrix(mp1);
+	      #else
+	      mtmp->TrLessAntiHermMatrix();
+	      #endif
+	      mtmp++;
+	    }
+	}
+      ForceFlops += vol*60;
+
+  int x[4];
+  
+  for(x[0] = 0; x[0] < GJP.XnodeSites(); ++x[0]) {
+    for(x[1] = 0; x[1] < GJP.YnodeSites(); ++x[1]) {
+      for(x[2] = 0; x[2] < GJP.ZnodeSites(); ++x[2]) {
+	for(x[3] = 0; x[3] < GJP.TnodeSites(); ++x[3]) {
+	  
+	  //Calculates the array index offset for the gauge links
+	  //located at lattice point x
+	  int uoff = GsiteOffset(x);
+	  
+	  for (int mu = 0; mu < 4; ++mu) {
+//	    GforceSite(*mp0, x, mu);   //Old force calculation
+	    
+	    IFloat *ihp = (IFloat *)(mom+uoff+mu);  //The gauge momentum
+	    IFloat *dotp = (IFloat *)mp0;
+	    IFloat *dotp2 = (IFloat *) (result[mu]+(uoff/4));
+	    fTimesV1PlusV2(ihp, step_size, dotp2, ihp, 18);  //Update the gauge momentum
+	  }
+	}
+      }
+    }
+  }
+  ForceFlops += vol*144;
+#ifdef PROFILE
+  time += dclock();
+  print_flops(cname,fname,ForceFlops+ParTrans::PTflops,time);
+#endif
+
+  //Free some memory
+  ffree(Unit);
+  for(int i = 0;i<N;i++){
+  ffree(tmp1[i]);
+  ffree(tmp2[i]);
+  }
+  for(int i = 0;i<4;i++) 
+  ffree(result[i]);
+}
+CPS_END_NAMESPACE
