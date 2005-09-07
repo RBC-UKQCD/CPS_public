b559005dcfe91df41ebcc488a5c23f744cb8049a
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2005-09-07 19:03:10 +0000

    *** empty log message ***
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@1960 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/include/util/enum_func.h
@@ -0,0 +1,12 @@
+#include<config.h>
+
+#ifndef INCLUDED_ENUM_FUNC_H
+#define INCLUDED_ENUM_FUNC_H           //!< Prevent multiple inclusion.
+
+#include <util/enum.h>
+CPS_START_NAMESPACE
+
+int NumChkb( RitzMatType ritz_mat);
+
+CPS_END_NAMESPACE
+#endif
