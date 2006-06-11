9f3f9b04aee7cd10335951895f7ffed0d1f5894e
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2006-06-11 03:42:42 +0000

    merging v4_9_8_mike_release2_michaelc_1_CJ
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@2411 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/tests/vml_templates/templates.C
+++ cps_pp/tests/vml_templates/templates.C
@@ -9,6 +9,7 @@
 #include<alg/pbp_arg.h>
 #include<alg/array_arg.h>
 #include<alg/fix_gauge_arg.h>
+#include<alg/s_spect_arg.h>
 
 #include<alg/hmc_arg.h>
 #include<alg/int_arg.h>
@@ -41,7 +42,13 @@ ActionGaugeArg gauge_arg;
 IntABArg ab_arg;
 EigArg eig_arg;
 FloatArray float_array;
-
+StagQuarkSrc stag_quark_src;
+StagQuarkArg stag_quark_arg;
+StagMesonArg stag_meson_arg;
+StagMomMesonArg stag_mom_meson_arg;
+StagNucleonArg stag_nucleon_arg;
+StagNonLocalArg stag_non_local_arg;
+NLStagMesonArg nlstag_meson_arg;
 
 int main(int argc, char *argv[])
 { 
@@ -89,6 +96,46 @@ int main(int argc, char *argv[])
   rat_quo_arg.resize(1);
   rat_quo_arg.resize(0, 9, 15, 6, 9);
   float_array.resize(4);
+  stag_quark_src.type = POINT;
+  for(int i = 0; i < 4; i++)
+    {
+      stag_quark_src.origin[i] = 0;
+      stag_quark_src.end[i] = 0;
+    }
+  stag_quark_src.dir = HDM_T;
+  stag_quark_arg.qid = 0;
+  stag_quark_arg.cg.mass = 0.1;
+  stag_quark_arg.cg.max_num_iter = 5000;
+  stag_quark_arg.cg.stop_rsd = 1e-10;
+  stag_quark_arg.cg.true_rsd = 1e-10;
+  stag_quark_arg.cg.RitzMatOper = MATPCDAG_MATPC;
+  stag_quark_arg.cg.Inverter = CG;
+  stag_quark_arg.cg.bicgstab_n = 1;
+  stag_quark_arg.src = stag_quark_src;
+  stag_quark_arg.sln = LOCAL;
+  stag_meson_arg.qid0 = 0;
+  stag_meson_arg.qid1 = 1;
+  stag_meson_arg.dir = HDM_T;
+  stag_meson_arg.meson_buf = 0;
+  stag_mom_meson_arg.qid0 = 0;
+  stag_mom_meson_arg.qid1 = 1;
+  stag_mom_meson_arg.dir = HDM_T;
+  stag_mom_meson_arg.no_of_momenta = 4;
+  stag_mom_meson_arg.meson_buf = 0;
+  stag_nucleon_arg.qid0 = 0;
+  stag_nucleon_arg.qid1 = 1;
+  stag_nucleon_arg.qid2 = 2;
+  stag_nucleon_arg.dir = HDM_T;
+  stag_nucleon_arg.nucleon_buf = 0;
+  stag_non_local_arg.qid0 = 0;
+  stag_non_local_arg.qid1 = 1;
+  stag_non_local_arg.qid2 = 2;
+  stag_non_local_arg.dir = HDM_T;
+  stag_non_local_arg.nlocal_buf = 0;
+  for(int i = 0; i < 8; i++)
+    nlstag_meson_arg.qid0[i] = i;
+  nlstag_meson_arg.dir = HDM_T;
+  nlstag_meson_arg.nlocal_buf = 0;
 
   cg_arg.Encode("cg_arg.vml","cg_arg");
   hmd_arg.Encode("hmd_arg.vml","hmd_arg");
@@ -111,6 +158,13 @@ int main(int argc, char *argv[])
   ab_arg.Encode("ab_arg.vml","ab_arg");
   eig_arg.Encode("eig_arg.vml","eig_arg");
   float_array.Encode("float_array.vml","float_array");
+  stag_quark_src.Encode("stag_quark_src.vml","stag_quark_src");
+  stag_quark_arg.Encode("stag_quark_arg.vml","stag_quark_arg");
+  stag_meson_arg.Encode("stag_meson_arg.vml","stag_meson_arg");
+  stag_mom_meson_arg.Encode("stag_mom_meson_arg.vml","stag_mom_meson_arg");
+  stag_nucleon_arg.Encode("stag_nucleon_arg.vml","stag_nucleon_arg");
+  stag_non_local_arg.Encode("stag_non_local_arg.vml","stag_non_local_arg");
+  nlstag_meson_arg.Encode("nlstag_meson_arg.vml","nlstag_meson_arg");
 
   vml_markup_type(VML_XML);
   
@@ -130,6 +184,13 @@ int main(int argc, char *argv[])
   gauge_arg.Encode("gauge_arg.xml","gauge_arg");
   ab_arg.Encode("ab_arg.xml","ab_arg");
   eig_arg.Encode("eig_arg.xml","eig_arg");
+  stag_quark_src.Encode("stag_quark_src.xml","stag_quark_src");
+  stag_quark_arg.Encode("stag_quark_arg.xml","stag_quark_arg");
+  stag_meson_arg.Encode("stag_meson_arg.xml","stag_meson_arg");
+  stag_mom_meson_arg.Encode("stag_mom_meson_arg.xml","stag_mom_meson_arg");
+  stag_nucleon_arg.Encode("stag_nucleon_arg.xml","stag_nucleon_arg");
+  stag_non_local_arg.Encode("stag_non_local_arg.xml","stag_non_local_arg");
+  nlstag_meson_arg.Encode("nlstag_meson_arg.xml","nlstag_meson_arg");
 
   return(0);
 }
