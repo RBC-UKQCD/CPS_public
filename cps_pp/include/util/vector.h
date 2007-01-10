68033c093df8a3c7fb4ae489c4e2afc87d9deab9
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2007-01-10 17:53:37 +0000

    Fixes for QMP on BG/L
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@2580 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/include/util/vector.h
+++ cps_pp/include/util/vector.h
@@ -1,35 +1,31 @@
+#ifndef INCLUDED_VECTOR_H
+#define INCLUDED_VECTOR_H               //!< Prevent multiple inclusion
 #include<config.h>
-CPS_START_NAMESPACE
 /*!\file
   \brief  Declaration/definition of Vector and Matrix classes.
 
   Also declarations of functions that perform operations on complex vectors.
 
-  $Id: vector.h,v 1.22 2006-12-21 20:40:29 chulwoo Exp $
+  $Id: vector.h,v 1.23 2007-01-10 17:53:36 chulwoo Exp $
 */
 //--------------------------------------------------------------------
 //  CVS keywords
 //
 //  $Author: chulwoo $
-//  $Date: 2006-12-21 20:40:29 $
-//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/include/util/vector.h,v 1.22 2006-12-21 20:40:29 chulwoo Exp $
-//  $Id: vector.h,v 1.22 2006-12-21 20:40:29 chulwoo Exp $
+//  $Date: 2007-01-10 17:53:36 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/include/util/vector.h,v 1.23 2007-01-10 17:53:36 chulwoo Exp $
+//  $Id: vector.h,v 1.23 2007-01-10 17:53:36 chulwoo Exp $
 //  $Name: not supported by cvs2svn $
 //  $Locker:  $
 //  $RCSfile: vector.h,v $
-//  $Revision: 1.22 $
+//  $Revision: 1.23 $
 //  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/include/util/vector.h,v $
 //  $State: Exp $
 //
 //--------------------------------------------------------------------
-
-
-#ifndef INCLUDED_VECTOR_H
-#define INCLUDED_VECTOR_H               //!< Prevent multiple inclusion
-
-CPS_END_NAMESPACE
 #include <string.h>
 #include <util/data_types.h>
+#include <util/vector_asm.h>
 CPS_START_NAMESPACE
 
 
@@ -656,42 +652,5 @@ class Vector
 
 };
 
-#if TARGET == QCDOC ||  TARGET == BGL || ( defined USE_QMP )
-
-extern "C" {
-  void xaxpy (Float *scalep,Float *InOutScale, Float *Add, int len);
-  void xaxpy_norm (Float *scalep, Float *InOutScale, Float *Add, int len, Float *res);
-  
-  void vaxpy3(Vector *res,Float *scale,Vector *mult,Vector *add, int ncvec);
-  inline void vaxpy3_m(Matrix *res,Float *scale,Matrix *mult,const Matrix *add, int ncvec){
-    vaxpy3((Vector *)res, scale, (Vector *)mult,(Vector *)add,ncvec);
-  }
-
-  void vaxpy3_norm(Vector *res,Float *scale,Vector *mult,Vector *add, int ncvec,Float *norm);
-  inline void vaxpy3_norm_m(Vector *res,Float *scale,Vector *mult,Vector *add, int ncvec,Float *norm){
-    vaxpy3_norm((Vector *)res, scale, (Vector *)mult,(Vector *)add,ncvec,norm);
-  }
-
-  // SU(3) * SU(3) streaming operations
-  void m1m2(Matrix *res, const Matrix *m1, const Matrix *m2, int *length);
-  void m1m2dag(Matrix *res, const Matrix *m1, const Matrix *m2, int *length);
-  void m1dagm2dag(Matrix *res, const Matrix *m1, const Matrix *m2, int *length);
-  void gdagmdag(Matrix *res, const Matrix *g, const Matrix *m, int *length);
-
-  // f*SU(3)*SU(3) + SU(3) streaming operations
-  void fm1m2pm3(Matrix *res, Float *scale, const Matrix *m1, 
-		const Matrix *m2, Matrix *m3, int *length);
-  void fm1m2dagpm3(Matrix *res, Float *scale, const Matrix *m1, 
-		   const Matrix *m2, Matrix *m3, int *length);
-  void fm1dagm2dagpm3(Matrix *res, Float *scale, const Matrix *m1, 
-		      const Matrix *m2, Matrix *m3, int *length);
-  void fgdagm1dagpm2(Matrix *res, Float *scale, const Matrix *g, 
-		     const Matrix *m1, Matrix *m2, int *length);
-
-}
-
-#endif
-#endif
-
-
 CPS_END_NAMESPACE
+#endif
