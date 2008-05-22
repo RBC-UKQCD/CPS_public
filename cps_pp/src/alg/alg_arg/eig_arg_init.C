866e1b1f089136999b37207db6eeaa61a5e61535
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2008-05-22 16:18:06 +0000

    Adding destructor & resize() for EigArg
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@2966 fb734a29-28db-4bc5-88a3-3cb5ee595545

---
+++ cps_pp/src/alg/alg_arg/eig_arg_init.C
@@ -0,0 +1,24 @@
+#include <alg/eig_arg.h>
+
+CPS_START_NAMESPACE
+
+EigArg::EigArg(){
+  Mass.Mass_len=0;
+  Mass.Mass_val=NULL;
+}
+
+EigArg::~EigArg(){
+  if(Mass.Mass_val)
+    delete[] Mass.Mass_val;
+}
+
+void EigArg::resize ( u_int n_floats){
+  Mass.Mass_len=n_floats;
+  if(Mass.Mass_val)
+    delete[] Mass.Mass_val;
+  if(n_floats>0)
+    Mass.Mass_val= new Float[n_floats];
+  else
+    Mass.Mass_val= NULL;
+}
+CPS_END_NAMESPACE
