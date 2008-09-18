f8cfdbc5cde526417bb146e65362336ae09e3375
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2008-09-18 14:46:52 +0000

    *** empty log message ***
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@3152 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/src/comms/qmp/cbuf/cram.C
+++ cps_pp/src/comms/qmp/cbuf/cram.C
@@ -4,12 +4,12 @@ CPS_START_NAMESPACE
 //  CVS keywords
 //
 //  $Author: chulwoo $
-//  $Date: 2006-11-25 19:10:04 $
-//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/comms/qmp/cbuf/cram.C,v 1.2 2006-11-25 19:10:04 chulwoo Exp $
-//  $Id: cram.C,v 1.2 2006-11-25 19:10:04 chulwoo Exp $
+//  $Date: 2008-09-18 14:46:52 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/comms/qmp/cbuf/cram.C,v 1.3 2008-09-18 14:46:52 chulwoo Exp $
+//  $Id: cram.C,v 1.3 2008-09-18 14:46:52 chulwoo Exp $
 //  $Name: not supported by cvs2svn $
 //  $Locker:  $
-//  $Revision: 1.2 $
+//  $Revision: 1.3 $
 //  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/comms/qmp/cbuf/cram.C,v $
 //  $State: Exp $
 //
@@ -19,7 +19,7 @@ CPS_END_NAMESPACE
 CPS_START_NAMESPACE
 //Allocate space for the SCRATCH CRAM 
 //Workstation version only
-#ifndef _TARTAN
+#if 0
 int CRAM_SCRATCH_INTS[CRAM_SCRATCH_SIZE] ;
 unsigned long CRAM_SCRATCH_ADDR = (unsigned int)CRAM_SCRATCH_INTS ;
 #endif
