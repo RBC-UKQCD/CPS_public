780da59c52af1c05f12e42acbacb23d1476a5652
chulwoo <chulwoo@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2013-05-14 16:56:34 +0000

    Added missing lattice types constructor/destructors deleted explicit #include <omp.h> and omp_set_num_threads().
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/trunk@4563 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/src/util/dirac_op/d_op_wilson/qmp/wilson_dslash_vec.C
+++ cps_pp/src/util/dirac_op/d_op_wilson/qmp/wilson_dslash_vec.C
@@ -1,7 +1,7 @@
 #include <config.h>
 #include <stdio.h>
 #include <math.h>
-#include <omp.h>
+#include <util/omp_wrapper.h>
 #include <qmp.h>
 
 #ifdef USE_BFM
@@ -12,18 +12,18 @@ CPS_START_NAMESPACE
 /*! \file
   \brief  Routine used internally in the DiracOpWilson class.
 
-  $Id: wilson_dslash_vec.C,v 1.4 2013-04-24 21:16:13 chulwoo Exp $
+  $Id: wilson_dslash_vec.C,v 1.5 2013-05-14 16:56:34 chulwoo Exp $
 */
 //--------------------------------------------------------------------
 //  CVS keywords
 //
 //  $Author: chulwoo $
-//  $Date: 2013-04-24 21:16:13 $
-//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/util/dirac_op/d_op_wilson/qmp/wilson_dslash_vec.C,v 1.4 2013-04-24 21:16:13 chulwoo Exp $
-//  $Id: wilson_dslash_vec.C,v 1.4 2013-04-24 21:16:13 chulwoo Exp $
+//  $Date: 2013-05-14 16:56:34 $
+//  $Header: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/util/dirac_op/d_op_wilson/qmp/wilson_dslash_vec.C,v 1.5 2013-05-14 16:56:34 chulwoo Exp $
+//  $Id: wilson_dslash_vec.C,v 1.5 2013-05-14 16:56:34 chulwoo Exp $
 //  $Name: not supported by cvs2svn $
 //  $Locker:  $
-//  $Revision: 1.4 $
+//  $Revision: 1.5 $
 //  $Source: /home/chulwoo/CPS/repo/CVS/cps_only/cps_pp/src/util/dirac_op/d_op_wilson/qmp/wilson_dslash_vec.C,v $
 //  $State: Exp $
 //
@@ -593,7 +593,7 @@ Printf("getMinusData((IFloat *)fbuf, (IFloat *)tmp6, SPINOR_SIZE, 1);\n");
 	setup += time;
 
 	time = -dclock();
-	omp_set_num_threads(64);
+//	omp_set_num_threads(64);
 /*--------------------------------------------------------------------------*/
 /* Loop over sites                                                          */
 /*--------------------------------------------------------------------------*/
